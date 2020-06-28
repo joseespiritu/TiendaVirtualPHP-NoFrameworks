90f5416d89f2e06202d533d0358eaeef0385ebaf
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-06-28 11:22:13 -0500

    Registro usuarios completado

--- views/usuario/registro.php
+++ views/usuario/registro.php
@@ -1,10 +1,12 @@
 <h1>Registrarse</h1>
 
-<?php if(isset($_SESSION['register']) && $_SESSION['register']): ?>
-    <strong>Registro completado correctamente</strong>
-<?php else: ?>
-    <strong>Registro fallido</strong>
+<?php if(isset($_SESSION['register']) && $_SESSION['register'] == 'complete'): ?>
+    <strong class="alert_green">Registro completado correctamente</strong>
+<?php elseif(isset($_SESSION['register']) && $_SESSION['register'] == 'failed'): ?>
+    <strong class="alert_red">Registro fallido, Introduce bien los datos</strong>
 <?php endif; ?>
+
+<?php Utils::deleteSession('register'); ?>
 
 
 <form action="<?=BASE_URL?>usuario/save" method="POST">
