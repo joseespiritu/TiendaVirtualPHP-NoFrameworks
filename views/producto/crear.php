bfa3c4f312dd8ef56b7fa2ed32da709461ca5b2c
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-06-29 20:00:45 -0500

    Formulario para crear productos

---
+++ views/producto/crear.php
@@ -0,0 +1,35 @@
+<h1>Crear nuevos productos</h1>
+
+<div class="form-container">
+
+    <form action="<?= BASE_URL ?>producto/save" method="POST">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" />
+
+        <label for="descripcion">Descripción</label>
+        <textarea name="descripcion"></textarea>
+
+        <label for="precio">Precio</label>
+        <input type="text" name="precio" />
+
+        <label for="stock">Stock</label>
+        <input type="number" name="stock" />
+
+        <label for="categoria">Categoria</label>
+        <?php $categorias = Utils::showCategorias(); ?>
+        <select name="categoria">
+            <?php while ($cat = $categorias->fetch_object()) : ?>
+                <option value="<?= $cat->id ?>">
+                    <?= $cat->nombre ?>
+                </option>
+            <?php endwhile; ?>
+        </select>
+
+        <label for="imagen">Imagen</label>
+        <input type="file" name="imagen" />
+
+        <input type="submit" value="Guardar"/>
+
+    </form>
+
+</div>
