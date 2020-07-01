61390ae24a0b8c6f1847d8bc3f0872cc4e55f83d
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-06-30 21:24:13 -0500

    Maquetacion CSS pagina producto

--- views/producto/ver.php
+++ views/producto/ver.php
@@ -1,13 +1,19 @@
 <?php if (isset($product)) : ?>
     <h1><?= $product->nombre ?></h1>
-    <?php if ($product->imagen != null) : ?>
-        <img src="<?= BASE_URL ?>uploads/images/<?= $product->imagen ?>" alt="producto" />
-    <?php else : ?>
-        <img src="<?= BASE_URL ?>assets/img/camiseta.png" alt="producto" />
-    <?php endif; ?>
-    <p><?= $product->descripcion ?></p>
-    <p><?= $product->precio ?></p>
-    <a href="#" class="button">Comprar</a>
+    <div id="detail-product">
+        <div class="image">
+            <?php if ($product->imagen != null) : ?>
+                <img src="<?= BASE_URL ?>uploads/images/<?= $product->imagen ?>" alt="producto" />
+            <?php else : ?>
+                <img src="<?= BASE_URL ?>assets/img/camiseta.png" alt="producto" />
+            <?php endif; ?>
+        </div>
+        <div class="data">
+            <p class="description"><?= $product->descripcion ?></p>
+            <p class="price"><?= $product->precio ?>$</p>
+            <a href="#" class="button">Comprar</a>
+        </div>
+    </div>
 <?php else : ?>
     <h1>El producto no existe</h1>
 <?php endif; ?>
