0d5ba0c3a427f2785742c91a568e566bce8f6994
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-07-04 19:43:28 -0500

    BD Final

--- database/tienda_php_poo.sql
+++ database/tienda_php_poo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-06-2020 a las 15:00:08
+-- Tiempo de generación: 05-07-2020 a las 02:42:59
 -- Versión del servidor: 10.3.16-MariaDB
 -- Versión de PHP: 7.3.7
 
@@ -55,8 +55,24 @@ INSERT INTO `categorias` (`id`, `nombre`) VALUES
 CREATE TABLE `lineas_pedidos` (
   `id` int(255) NOT NULL,
   `pedido_id` int(255) NOT NULL,
-  `producto_id` int(255) NOT NULL
+  `producto_id` int(255) NOT NULL,
+  `unidades` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `lineas_pedidos`
+--
+
+INSERT INTO `lineas_pedidos` (`id`, `pedido_id`, `producto_id`, `unidades`) VALUES
+(1, 3, 5, 1),
+(2, 4, 5, 1),
+(3, 5, 5, 1),
+(4, 6, 5, 1),
+(5, 6, 6, 1),
+(6, 6, 7, 1),
+(7, 6, 2, 1),
+(8, 7, 7, 3),
+(9, 7, 6, 3);
 
 -- --------------------------------------------------------
 
@@ -75,6 +91,17 @@ CREATE TABLE `pedidos` (
   `fecha` date DEFAULT NULL,
   `hora` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id`, `usuario_id`, `provincia`, `localidad`, `direccion`, `coste`, `estado`, `fecha`, `hora`) VALUES
+(3, 6, 'Madrid', 'Madrid', 'Alcala nÂ°1 Piso 2, puerta c', 50.00, 'confirm', '2020-07-02', '19:12:10'),
+(4, 6, 'Madrid', 'Madrid', 'Alcala nÂ°1 Piso 2, puerta c', 50.00, 'confirm', '2020-07-02', '19:19:15'),
+(5, 6, 'Madrid', 'Madrid', 'Alcala nÂ°1 Piso 2, puerta c', 50.00, 'confirm', '2020-07-04', '17:43:18'),
+(6, 6, 'Madrid', 'Madrid', 'Alcala nÂ°1 Piso 2, puerta c', 100.00, 'ready', '2020-07-04', '18:34:21'),
+(7, 8, 'Barcelona', 'El prat', 'Calle Gran Via 8 Piso 1, puerta 6', 90.00, 'sended', '2020-07-04', '19:35:08');
 
 -- --------------------------------------------------------
 
@@ -99,7 +126,11 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `categoria_id`, `nombre`, `descripcion`, `precio`, `stock`, `oferta`, `fecha`, `imagen`) VALUES
-(1, 1, 'Box logo supreme', 'Box logo supreme dd', 100.00, 20, NULL, '2020-06-29', NULL);
+(1, 1, 'Box logo supreme', 'Box logo supreme dd', 100.00, 20, NULL, '2020-06-29', NULL),
+(2, 1, 'Camiseta Vans', 'Vans gris poliester', 20.00, 2333, NULL, '2020-06-29', NULL),
+(5, 7, 'Camiseta PUMA 5', 'Camiseta PUMA DSFF 3242', 50.00, 50, NULL, '2020-06-30', 'qqq.jpg'),
+(6, 1, 'Camiseta Rayas Azules', 'Camiseta Rayas Azules desc', 10.00, 20, NULL, '2020-07-04', 'ghd.jpg'),
+(7, 3, 'Camiseta Nike', 'Camiseta Nike Desc', 20.00, 20, NULL, '2020-07-04', 'uuu.jpg');
 
 -- --------------------------------------------------------
 
@@ -128,7 +159,8 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `rol`,
 (4, 'frefre', 'fref', 'MAIL@mail.com', '$2y$04$z02oa9AD40s96CJxkPQpnu1L1g2J/aKUyueW/qu/hjp7q6IliOdk.', 'user', 'null'),
 (5, 'fregfe', 'fergf', 'mai@mvre.com', '$2y$04$Qs4WuJXpZcZmdN8P90kk2ufBTNaoXHHACUyOzfdq1C1Q.zqPbLvYa', 'user', 'null'),
 (6, 'juan', 'lopez', 'juan@juan.com', '$2y$04$GJ/BTXlGYEgVdQbVih1XEuu8reegU5vLGooNkPVK8Vlf4uBGi24/q', 'admin', 'null'),
-(7, 'paco', 'paco', 'paco@paco.com', '$2y$04$FeHjYkO99xA07UpzqQ1m/OUpvAB3uNnlBvvxSpRxh0cC1VD3KsIfW', 'user', 'null');
+(7, 'paco', 'paco', 'paco@paco.com', '$2y$04$FeHjYkO99xA07UpzqQ1m/OUpvAB3uNnlBvvxSpRxh0cC1VD3KsIfW', 'user', 'null'),
+(8, 'David', 'Lopez', 'david@david.com', '$2y$04$OgN5oythLqn9Akig2jH8..b0MxbE6vT0mA67yPP2QB7Cy0nu73/Ye', 'user', 'null');
 
 --
 -- Índices para tablas volcadas
@@ -183,25 +215,25 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `lineas_pedidos`
 --
 ALTER TABLE `lineas_pedidos`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
